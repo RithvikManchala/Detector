5faffcf6fcc5982a8c55c2417060e7f321d4ecd6
RithvikManchala <87528192+RithvikManchala@users.noreply.github.com>
2022-04-21 17:51:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ objects = [];
 
 function preload()
 {
-    img = loadImage("Ferrari.png")
+    img = loadImage("image.jpg")
 }
 
 function setup()
@@ -31,15 +31,27 @@ function gotResults(error, results)
     else
     {
         console.log(results);
+        objects = results;
     }
 }
 
 function draw()
 {
     image(img, 0, 0, 640, 500);
-    fill("#ff0000");
-    text("Ferrari", 45, 75);
-    noFill();
-    stroke("#ff0000");
-    rect(30, 70, 444, 360);
+
+    if(status != "")
+   {
+       for(i = 0; i < objects.length; i++)
+       {
+           document.getElementById("status").innerHTML = "Status : Object Detected";
+
+           fill("#FF0000");
+           percent = floor(objects[i].confidence * 100);
+           text(objects[i].label  + " " + percent + "%" , objects[i].x+15, objects[i].y+15);
+
+           noFill();
+           stroke("#FF0000");
+           rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+       }
+   }
 }
